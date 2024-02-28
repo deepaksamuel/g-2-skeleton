77a73c1656b09cb24d5aba2e6aee6e63c95f31d7
Deepak Samuel <deepaksamuel@gmail.com>
2024-02-28 12:02:48 +0530

    second

---
+++ g2esoft-root-libs/LinkDef.h
@@ -0,0 +1,37 @@
+#ifdef __CLING__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class g2esoft::MCParticle+;
+#pragma link C++ class g2esoft::SimHit+;
+#pragma link C++ class g2esoft::MCStep+;
+#pragma link C++ class g2esoft::StripHit+;
+#pragma link C++ class g2esoft::StripCluster+;
+#pragma link C++ class g2esoft::RecoHit+;
+#pragma link C++ class g2esoft::Track+;
+#pragma link C++ class std::vector<g2esoft::MCStep>+;
+#pragma link C++ class std::vector<g2esoft::MCStep*>+;
+#pragma link C++ class std::vector<g2esoft::SimHit*>+;
+#pragma link C++ class std::vector<g2esoft::MCParticle*>+;
+#pragma link C++ class std::vector<g2esoft::StripHit*>+;
+#pragma link C++ class std::vector<g2esoft::StripCluster*>+;
+#pragma link C++ class std::vector<g2esoft::RecoHit*>+;
+#pragma link C++ class std::vector<g2esoft::Track*>+;
+#pragma link C++ class std::vector<const g2esoft::MCStep*>+;
+#pragma link C++ class std::vector<const g2esoft::SimHit*>+;
+#pragma link C++ class std::vector<const g2esoft::MCParticle*>+;
+#pragma link C++ class std::vector<const g2esoft::StripHit*>+;
+#pragma link C++ class std::vector<const g2esoft::StripCluster*>+;
+#pragma link C++ class std::vector<const g2esoft::RecoHit*>+;
+#pragma link C++ class std::vector<const g2esoft::Track*>+;
+#pragma link C++ class g2esoft::const_ref_vector<g2esoft::MCStep>+;
+#pragma link C++ class g2esoft::const_ref_vector<g2esoft::MCParticle>+;
+#pragma link C++ class g2esoft::const_ref_vector<g2esoft::SimHit>+;
+#pragma link C++ class g2esoft::const_ref_vector<g2esoft::StripHit>+;
+#pragma link C++ class g2esoft::const_ref_vector<g2esoft::StripCluster>+;
+#pragma link C++ class g2esoft::const_ref_vector<g2esoft::RecoHit>+;
+#pragma link C++ class g2esoft::const_ref_vector<g2esoft::Track>+;
+
+#endif
